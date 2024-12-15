756a85115f57ad5410ec81c6cf6439de140e7339
Urooj Saeed <165631956+Urooj1112@users.noreply.github.com>
2024-12-15 07:58:44 -0800

    Add files via upload

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,18 @@
+const Hero = () => {
+  return (
+    <main>
+<section className="flex justify-center gap-5 h-auto w-full p-4 mt-8">
+<div className="h-80 w-[450px] bg-blue-500 rounded-md shadow-2xl"></div>
+<div className="h-80 w-[450px] bg-red-500 rounded-md shadow-2xl"></div>
+<div className="h-80 w-[450px] bg-gray-500 rounded-md shadow-2xl"></div>
+</section>
+
+<section className=" flex justify-center gap-2 h-auto w-full p-4 mt-8">
+<div className="h-80 w-[450px] bg-pink-500 rounded-md shadow-2xl"></div>
+<div className="h-80 w-[450px] bg-orange-500 rounded-md shadow-2xl"></div>
+</section>
+</main>
+  )
+}
+
+export default Hero
